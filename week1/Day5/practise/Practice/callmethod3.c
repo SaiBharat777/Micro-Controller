64f0e4b4139932109596225df34534a24ecce9e9
SaiBharat777 <saibharatnomula@gmail.com>
2023-03-31 11:17:08 +0530

    Commit Week1 in repo

---
+++ week1/Day5/practise/Practice/callmethod3.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(int argc, char *argv[], char *envp[])
+{
+	void function_country();
+	function_country();
+	return(0);
+}
+void function_country(void)
+{
+	void function_OfAMC(void);
+	function_OfAMC();
+	printf("\nI live in India.\n");
+}
+void function_OfAMC(void)
+{
+	void function_surname(void);
+	function_surname();
+	printf("Of Phytech");
+}
+void function_surname(void)
+{
+	void function_middlename(void);
+	function_middlename();
+	printf
